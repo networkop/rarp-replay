3a2bede205e86bb6e4d9c0fe393b42e6c1b17fd6
networkop <mmkashin@gmail.com>
2023-01-05 22:20:22 +0000

    reworked rarp reply logic

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 
 fire:
-	sudo go run cmd/request/main.go
+	sudo go run cmd/request/main.go -mac 44:38:39:22:01:23 -intf eth0
 
 listen:
 	sudo go run main.go
--- main.go
+++ main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"net/netip"
 	"sync"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"github.com/mdlayher/arp"
+	"github.com/mdlayher/ethernet"
 )
 
-func rarp(intf *net.Interface) error {
+func inARP(rarp gopacket.Packet, intf *net.Interface, ip netip.Addr) ([]byte, error) {
+	// gopacket does not understand RARP so using arp package instead
+	var rarpFrame ethernet.Frame
+	if err := rarpFrame.UnmarshalBinary(rarp.Data()); err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal into ethernet packet: %s", err)
+	}
+	var rarpPayload arp.Packet
+	if err := rarpPayload.UnmarshalBinary(rarpFrame.Payload); err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal into arp packet: %s", err)
+	}
+
+	p, err := arp.NewPacket(
+		arp.OperationRequest,           // Operation
+		intf.HardwareAddr,              // srcHW
+		ip,                             // srcIP
+		rarpPayload.TargetHardwareAddr, // dstHW
+		netip.MustParseAddr("0.0.0.0"), // dstIP
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to arp.NewPacket: %v", err)
+	}
+	log.Println(rarp)
+
+	inarpBinary, err := p.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("error serializing RARP packet: %s", err)
+	}
+
+	ethFrame := &ethernet.Frame{
+		Destination: ethernet.Broadcast,
+		Source:      intf.HardwareAddr,
+		EtherType:   ethernet.EtherTypeARP,
+		Payload:     inarpBinary,
+	}
+
+	return ethFrame.MarshalBinary()
+}
+
+func arpProbe(intf *net.Interface) error {
 
 	handle, err := pcap.OpenLive(
 		intf.Name,         // interface name
@@ -29,10 +71,29 @@ func rarp(intf *net.Interface) error {
 		handle,
 		layers.LayerTypeEthernet,
 	)
+
+	addrs, err := intf.Addrs()
+	if err != nil {
+		return err
+	}
+	var addr netip.Addr
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok {
+			if ip4 := ipnet.IP.To4(); ip4 != nil {
+				addr, _ = netip.AddrFromSlice(ip4)
+				break
+			}
+		}
+	}
+
 	for packet := range packetSource.Packets() {
 		log.Println("Found RARP packet")
 
-		if err := handle.WritePacketData(packet.Data()); err != nil {
+		probe, err := inARP(packet, intf, addr)
+		if err != nil {
+			return err
+		}
+		if err := handle.WritePacketData(probe); err != nil {
 			log.Fatalf("Failed to send packet data :%s\n", err)
 		}
 	}
@@ -50,7 +111,7 @@ func main() {
 		wg.Add(1)
 		go func(iface net.Interface) {
 			defer wg.Done()
-			if err := rarp(&iface); err != nil {
+			if err := arpProbe(&iface); err != nil {
 				log.Printf("interface %v: %v", iface.Name, err)
 			}
 		}(iface)
